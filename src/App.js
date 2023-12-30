126cb2bc70b740d67b4ee33a00d7d336a5dcdae3
RahaneSayali <sayalipr15@gmail.com>
2023-12-30 13:48:43 +0530

    section added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,15 @@ const App = () => {
         <AppContext>
             <BrowserRouter>
             <div className="flex flex-col h-full">
-
+            <Header />
+                    <Routes>
+                        <Route path="/" exact element={<Feed />} />
+                        <Route
+                            path="/searchResult/:searchQuery"
+                            element={<SearchResult />}
+                        />
+                        <Route path="/video/:id" element={<VideoDetails />} />
+                    </Routes>
             </div>
             </BrowserRouter>
         </AppContext>
---
+++ src/components/SuggestionVideoCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SuggestionVideoCard() {
+  return (
+    <div>SuggestionVideoCard</div>
+  )
+}
